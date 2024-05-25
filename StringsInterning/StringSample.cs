a81665cc336e88682398f4dce72a48fc1aac393e
Dennis Vroegop <dennis@vroegop.org>
2024-05-25 10:49:49 +0200

    Cleaning up

--- StringsInterning/StringSample.cs
+++ StringsInterning/StringSample.cs
@@ -1,48 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ExtensionsLibrary;
+﻿using ExtensionsLibrary;
 
-namespace StringsInterning
+namespace StringsInterning;
+
+internal class StringSample
 {
-    internal class StringSample
+    public void UseNonInterned()
     {
-        public void UseNonInterned()
-        {
-            const int numberOfStrings = 10000;
+        const int numberOfStrings = 10000;
 
-            // Array to hold non-interned strings
-            string[]? nonInternedStrings = new string[numberOfStrings];
+        // Array to hold non-interned strings
+        var nonInternedStrings = new string[numberOfStrings];
 
-            // Create a large number of identical strings
-            for (int i = 0; i < numberOfStrings; i++)
-            {
-                nonInternedStrings[i] = new string(new char[] { 'H', 'e', 'l', 'l', 'o' });
-            }
-            long memoryInUse = GC.GetTotalMemory(true);
-            $"Memory in use is {memoryInUse:###,###}".Dump();
-        }
+        // Create a large number of identical strings
+        for (var i = 0; i < numberOfStrings; i++) nonInternedStrings[i] = new string(new[] { 'H', 'e', 'l', 'l', 'o' });
+        var memoryInUse = GC.GetTotalMemory(true);
+        $"Memory in use is {memoryInUse:###,###}".Dump();
+    }
 
-        public void UseInterned()
-        {
-            const int numberOfStrings = 10000;
+    public void UseInterned()
+    {
+        const int numberOfStrings = 10000;
 
-            // Array to hold non-interned strings
-            string[]? internedStrings = new string[numberOfStrings];
+        // Array to hold non-interned strings
+        var internedStrings = new string[numberOfStrings];
 
-            internedStrings[0] = new string(new char[] { 'H', 'e', 'l', 'l', 'o' });
-            
-            // Create a large number of identical strings
-            for (int i = 1; i < numberOfStrings; i++)
-            {
-                internedStrings[i] = String.Intern(internedStrings[0]);
-            }
-            
-            long memoryInUse = GC.GetTotalMemory(true);
-            $"Memory in use is {memoryInUse:###,###}".Dump();
-        }
+        internedStrings[0] = new string(new[] { 'H', 'e', 'l', 'l', 'o' });
 
+        // Create a large number of identical strings
+        for (var i = 1; i < numberOfStrings; i++) internedStrings[i] = string.Intern(internedStrings[0]);
+
+        var memoryInUse = GC.GetTotalMemory(true);
+        $"Memory in use is {memoryInUse:###,###}".Dump();
     }
 }
