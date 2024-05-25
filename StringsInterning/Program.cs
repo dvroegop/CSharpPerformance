19aea6f5edd73e43c20d42eecc0c5bdda28ad60c
Dennis Vroegop <dennis@vroegop.org>
2024-05-25 10:48:15 +0200

    Fixed string sample

--- StringsInterning/Program.cs
+++ StringsInterning/Program.cs
@@ -1,48 +1,21 @@
-﻿// Number of strings to create
-
+﻿
 using ExtensionsLibrary;
+using StringsInterning;
 
-// Number of strings to create
-const int numberOfStrings = 10000;
+"Press 1 for non interned, press 2 for interned".Dump(ConsoleColor.Yellow);
 
-// Array to hold non-interned strings
-string[]? nonInternedStrings = new string[numberOfStrings];
-
-// Create a large number of identical strings
-for (int i = 0; i < numberOfStrings; i++)
+var choice = Console.ReadKey().KeyChar;
+if (choice == '1')
 {
-    nonInternedStrings[i] = new string(new char[] {'H', 'e', 'l', 'l', 'o'});
+    var stringSample = new StringSample();
+    stringSample.UseNonInterned();
 }
-
-// Check memory usage before interning
-GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive);
-GC.WaitForPendingFinalizers();
-GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive);
-long memoryBeforeInterning = GC.GetTotalMemory(true);
-
-// Array to hold interned strings
-string[] internedStrings = new string[numberOfStrings];
-
-// Intern all strings
-for (int i = 0; i < numberOfStrings; i++)
+else if (choice == '2')
 {
-    internedStrings[i] = String.Intern(nonInternedStrings[i]);
+    var stringSample = new StringSample();
+    stringSample.UseInterned();
 }
-
-nonInternedStrings = null;
-// Check memory usage after interning
-GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive);
-GC.WaitForPendingFinalizers();
-GC.Collect();
-long memoryAfterInterning = GC.GetTotalMemory(true);
-
-// Collect garbage and get final memory usage
-GC.Collect(GC.MaxGeneration, GCCollectionMode.Aggressive);
-GC.WaitForPendingFinalizers();
-GC.Collect();
-long memoryAfterGC = GC.GetTotalMemory(true);
-
-
-$"Memory before interning:\t {memoryBeforeInterning:###,###,###} bytes".Dump();
-$"Memory after interning:\t\t {memoryAfterInterning:###,###,###} bytes".Dump();
-$"Memory after GC: {memoryAfterGC} bytes".Dump();
+else
+{
+    "Invalid choice".Dump(ConsoleColor.Red);
+}
---
+++ StringsInterning/StringSample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionsLibrary;
+
+namespace StringsInterning
+{
+    internal class StringSample
+    {
+        public void UseNonInterned()
+        {
+            const int numberOfStrings = 10000;
+
+            // Array to hold non-interned strings
+            string[]? nonInternedStrings = new string[numberOfStrings];
+
+            // Create a large number of identical strings
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                nonInternedStrings[i] = new string(new char[] { 'H', 'e', 'l', 'l', 'o' });
+            }
+            long memoryInUse = GC.GetTotalMemory(true);
+            $"Memory in use is {memoryInUse:###,###}".Dump();
+        }
+
+        public void UseInterned()
+        {
+            const int numberOfStrings = 10000;
+
+            // Array to hold non-interned strings
+            string[]? internedStrings = new string[numberOfStrings];
+
+            internedStrings[0] = new string(new char[] { 'H', 'e', 'l', 'l', 'o' });
+            
+            // Create a large number of identical strings
+            for (int i = 1; i < numberOfStrings; i++)
+            {
+                internedStrings[i] = String.Intern(internedStrings[0]);
+            }
+            
+            long memoryInUse = GC.GetTotalMemory(true);
+            $"Memory in use is {memoryInUse:###,###}".Dump();
+        }
+
+    }
+}
