56829ab83bd33550838c9693fba4f3eebcff7328
Dennis Vroegop <dennis@vroegop.org>
2024-05-25 10:20:43 +0200

    Added benchmark Demo

---
+++ Benchmarking/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using BenchmarkDotNet.Running;
+using Benchmarking;
+
+var summary= BenchmarkRunner.Run<TestCases>();
+
+/*
+ * 
+ * 
+ * | Method      | Mean       | Error   | StdDev  |
+   |------------ |-----------:|--------:|--------:|
+   | AFastMethod |   107.7 ms | 0.81 ms | 0.75 ms |
+   | ASlowMethod | 1,008.1 ms | 4.49 ms | 4.20 ms |
+
+*/
---
+++ Benchmarking/TestCases.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarking
+{
+    public class TestCases
+    {
+        [Benchmark]
+        public void AFastMethod()
+        {
+            System.Threading.Thread.Sleep(100);   
+        }
+
+        [Benchmark]
+        public void ASlowMethod()
+        {
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
